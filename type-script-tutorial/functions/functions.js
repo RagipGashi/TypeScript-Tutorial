15d05ff358e2d95dadaaccde1a50d547a06f51dc
Ragip Gashi <ragipgashi@gmial.com>
2020-09-01 13:33:38 +0200

    #1 Added another function to the file functions

---
+++ type-script-tutorial/functions/functions.js
@@ -0,0 +1,24 @@
+/*
+function add(x: number, y: number): number {
+  return x + y;
+}
+
+// Anonymos function
+let myAdd = function (x: any, y: any): number {
+  if (typeof x == "string") {
+    x = parseInt(x, 10); // parse a string to integer with the base of 10
+  }
+  if (typeof y == "string") {
+    y = parseInt(y, 10);
+  }
+  return x + y;
+};
+
+// console.log(add(5, 5));
+console.log(myAdd("4", 4));
+*/
+function getFullName(firstName, lastName) {
+    return firstName + ' ' + lastName;
+}
+var name1 = getFullName('John', 'Doe');
+console.log(name1);
