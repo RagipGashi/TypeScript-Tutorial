62b3515e44246068a6a435ab5a347eb5ca689a76
Ragip Gashi <ragipgashi@gmial.com>
2020-09-01 14:19:20 +0200

    #1Added a function which when it is called, in call option use more parameters than declared in function.

--- type-script-tutorial/functions/functions.js
+++ type-script-tutorial/functions/functions.js
@@ -17,8 +17,23 @@ let myAdd = function (x: any, y: any): number {
 // console.log(add(5, 5));
 console.log(myAdd("4", 4));
 */
-function getFullName(firstName, lastName) {
+/*
+function getFullName (firstName: string, lastName?: string): string { // ? after the parameter is to save from error if we call the function with less parameters than declared
+    // To prevent from returning undefined
+    if (lastName == undefined) {
+        return firstName;
+    }
     return firstName + ' ' + lastName;
 }
-var name1 = getFullName('John', 'Doe');
+let name1 = getFullName('John');
+*/
+// If we don't know how many parameters we will call, just put ... in parameter list and in return use .join(' ');
+function getFullName(firstName) {
+    var restOfName = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        restOfName[_i - 1] = arguments[_i];
+    }
+    return firstName + ' ' + restOfName.join(' ');
+}
+var name1 = getFullName('John', 'Doe', 'Williams', 'Johnson', 'Jackson');
 console.log(name1);
--- type-script-tutorial/functions/functions.ts
+++ type-script-tutorial/functions/functions.ts
@@ -18,9 +18,22 @@ let myAdd = function (x: any, y: any): number {
 console.log(myAdd("4", 4));
 */
 
-function getFullName (firstName: string, lastName: string): string {
+/*
+function getFullName (firstName: string, lastName?: string): string { // ? after the parameter is to save from error if we call the function with less parameters than declared
+    // To prevent from returning undefined
+    if (lastName == undefined) {
+        return firstName;
+    }
     return firstName + ' ' + lastName;
 }
+let name1 = getFullName('John');
+*/
 
-let name1 = getFullName('John', 'Doe');
+// If we don't know how many parameters we will call, just put ... in parameter list and in return use .join(' ');
+function getFullName (firstName: string, ...restOfName: string[]) {
+    return firstName + ' ' + restOfName.join(' ');
+}
+
+let name1 = getFullName('John', 'Doe', 'Williams', 'Johnson', 'Jackson');
+
 console.log(name1);
