2ed69c97fe0db7cb0878e7e3f770864fcf2578c5
Ragip Gashi <ragipgashi@gmial.com>
2020-09-03 01:01:27 +0200

    #1 Added another example with union types

---
+++ type-script-tutorial/string_literal/string_literal.js
@@ -0,0 +1,28 @@
+/*
+// let x: 'Hello';
+
+// x = 'Hello'; -> works becuase it expected to be type Hello
+// x = 'Hello World'; -> doesn't work because it expected to be only the type of Hello
+// x = undefined; -> works just fine
+// x = null; -> works too
+
+let x: 'Hello' | 'Hello World' | 'Hello World!';
+
+x = 'Hello';
+x = 'Hello World';
+x = 'Hello World!'
+x = 'Hello World!!' //-> doesn't work: Type '"Hello World!!"' is not assignable to type '"Hello" | "Hello World" | "Hello World!"'.
+
+console.log(x);
+*/
+// Another example with union types
+function myAdd(x, y) {
+    if (typeof x == 'string') {
+        x = parseInt(x, 10);
+    }
+    if (typeof y == 'string') {
+        y = parseInt(y, 10);
+    }
+    return x + y;
+}
+console.log(myAdd(4, '4'));
