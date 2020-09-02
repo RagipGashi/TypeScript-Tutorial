83b62b66184e5d356ef95e35a1316c56e0379a33
Ragip Gashi <ragipgashi@gmial.com>
2020-09-03 00:32:13 +0200

    #1 Testing types void, undefined and never

---
+++ type-script-tutorial/void_undefined/void_undefined.ts
@@ -0,0 +1,31 @@
+/*
+function logData(): void {
+    console.log('Here is some data');
+    // return 'Some data'; // Doesn't work -> Type 'string' is not assignable to type 'void'.
+}
+
+logData();
+
+let u: undefined = undefined;
+let n: null = null;
+
+console.log(n);
+
+function error(message: string): never {
+    throw new Error(message);
+};
+
+error('Something happend');
+function infLoop(): never {
+    while(true) {
+        console.log('Hello');
+    }
+};
+
+function doSometing(): never {
+    return 'Hello'; // Don't work -> Type 'string' is not assignable to type 'never'.
+}
+
+*/
+
+
