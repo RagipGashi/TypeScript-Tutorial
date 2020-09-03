e376b99abb7eba493179813e203e21e8ea026142
Ragip Gashi <ragipgashi@gmial.com>
2020-09-04 00:57:23 +0200

    #1 Added examples with multiple types and arguments with generics

--- type-script-tutorial/generics/generics.js
+++ type-script-tutorial/generics/generics.js
@@ -1,21 +1,41 @@
+/*
 //// Using strict type of argument and the return type of the function
-function myStrictFunc(arg) {
+function myStrictFunc (arg: string): string {
     return arg;
 }
+
 //// Using any type of the arg and return
-function myAnyFunc(arg) {
+function myAnyFunc (arg: any): any {
     return arg;
 }
+
 //// Using GENERICS
-function myGenericFunc(arg) {
+function myGenericFunc<T> (arg: T): T {
     return arg;
 }
-var x = myGenericFunc('Hello World!');
-var y = myGenericFunc(40);
+
+let x = myGenericFunc<string> ('Hello World!');
+let y = myGenericFunc<number> (40);
 // With generics, it is possible to pass any type of argument without declaring it!!!
-var z = myGenericFunc('Hello');
-var w = myGenericFunc(44.44);
+let z = myGenericFunc('Hello');
+let w = myGenericFunc(44.44);
+
 console.log(x);
 console.log(y);
 console.log(z);
 console.log(w);
+*/
+//// Multiple arguments, multiple types
+function myGenericFunc(arg1, arg2) {
+    var str = arg1 + ' ' + arg2;
+    return str;
+}
+var x = myGenericFunc('Hello', 40);
+console.log(x);
+//// Using multiple types and arguments, but only with one type, <T>. Now in call we must pass the same type like in the first arg.
+function myGeneric(arg1, arg2) {
+    var str = arg1 + ' ' + arg2;
+    return str;
+}
+var y = myGeneric('Hello', 'World');
+console.log(y);
--- type-script-tutorial/generics/generics.ts
+++ type-script-tutorial/generics/generics.ts
@@ -1,3 +1,4 @@
+/*
 //// Using strict type of argument and the return type of the function
 function myStrictFunc (arg: string): string {
     return arg;
@@ -23,3 +24,22 @@ console.log(x);
 console.log(y);
 console.log(z);
 console.log(w);
+*/
+
+//// Multiple arguments, multiple types
+function myGenericFunc<T1, T2> (arg1, arg2) {
+    let str =  arg1 + ' ' + arg2;
+    return str;
+}
+
+let x = myGenericFunc('Hello', 40); 
+console.log(x);
+
+//// Using multiple types and arguments, but only with one type, <T>. Now in call we must pass the same type like in the first arg.
+function myGeneric<T> (arg1: T, arg2: T) {
+    let str =  arg1 + ' ' + arg2;
+    return str;
+}
+
+let y = myGeneric('Hello', 'World'); 
+console.log(y);
