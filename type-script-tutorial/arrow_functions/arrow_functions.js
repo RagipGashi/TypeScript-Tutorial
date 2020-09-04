1dfbd221ae20afc28b3502c10a004803241cadad
Ragip Gashi <ragipgashi@gmial.com>
2020-09-05 00:32:40 +0200

    #1 Added array_functions file and some code to see the differences and benefits

---
+++ type-script-tutorial/arrow_functions/arrow_functions.js
@@ -0,0 +1,12 @@
+/*
+//// Normal function
+let getName = function(name: string): string {
+    return name;
+}
+console.log(getName('Ragip Gashi'));
+
+//// ARROW function, with only one expression, we don't need to type return.
+let getName = (name: string):string => name;
+
+console.log(getName('Ragip Gashi'));
+*/ 
