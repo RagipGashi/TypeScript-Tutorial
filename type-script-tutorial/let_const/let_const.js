4ec89b5487896d88a5f821c5c1d5a7866eb5f47e
Ragip Gashi <ragipgashi@gmial.com>
2020-09-04 23:43:54 +0200

    #1 Added let_const folder and respective file

---
+++ type-script-tutorial/let_const/let_const.js
@@ -0,0 +1,6 @@
+/*
+let a = 55;
+let a = 44; // Error: Cannot redeclare block-scoped variable 'a'. This works with declaring as var and this is the main difference between var and let
+
+console.log(a);
+*/ 
