ca2f5bd297d80ca10a14d11e0089dabb4939cfd4
Ragip Gashi <ragipgashi@gmial.com>
2020-09-04 23:53:47 +0200

    #1 Added some code to see the difference between declaring variables with VAR and LET

--- type-script-tutorial/let_const/let_const.js
+++ type-script-tutorial/let_const/let_const.js
@@ -3,4 +3,24 @@ let a = 55;
 let a = 44; // Error: Cannot redeclare block-scoped variable 'a'. This works with declaring as var and this is the main difference between var and let
 
 console.log(a);
-*/ 
+*/
+//// VAR in a function
+function showVar() {
+    var a = 44;
+    if (true) {
+        var a = 33;
+        console.log(a); // This one has effected the global-scoped a variable and the result is two times 33
+    }
+    console.log(a);
+}
+//showVar();
+//// LET declared variable 
+function showLet() {
+    var a = 44;
+    if (true) {
+        var a_1 = 33;
+        console.log(a_1); // This one has NOT effected the global-scoped a variable and the result is 33 44
+    }
+    console.log(a);
+}
+//showLet();
--- type-script-tutorial/let_const/let_const.ts
+++ type-script-tutorial/let_const/let_const.ts
@@ -4,3 +4,28 @@ let a = 44; // Error: Cannot redeclare block-scoped variable 'a'. This works wit
 
 console.log(a);
 */
+
+//// VAR in a function
+function showVar() {
+    var a = 44;
+    if (true) {
+        var a = 33;
+        console.log(a); // This one has effected the global-scoped a variable and the result is two times 33
+    }
+    console.log(a);
+}
+//showVar();
+
+//// LET declared variable 
+function showLet() {
+    let a = 44;
+    if (true) {
+        let a = 33;
+        console.log(a); // This one has NOT effected the global-scoped a variable and the result is 33 44
+    }
+    console.log(a);
+}
+//showLet();
+
+
+
