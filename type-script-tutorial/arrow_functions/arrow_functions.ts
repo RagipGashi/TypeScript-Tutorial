00d947316246c75dc55bd790999bb527af98f36e
Ragip Gashi <ragipgashi@gmial.com>
2020-09-05 22:57:40 +0200

    #1 Added another example of arrow functions and some explanations in Explanations.md file

--- type-script-tutorial/arrow_functions/arrow_functions.ts
+++ type-script-tutorial/arrow_functions/arrow_functions.ts
@@ -1,5 +1,5 @@
 /*
-//// Normal function
+//// Regular function
 let getName = function(name: string): string {
     return name;
 }
@@ -19,6 +19,33 @@ let add = function(num1: number, num2: number): number {
 let add = (num1: number, num2: number): number => num1 + num2;
 
 console.log(add(1, 3));
-*/
 
-// 
+// Counting the age in one second in old way function
+var User = {
+    name: 'Ragip Gashi',
+    age: 40,
+    startAging: function() {
+        var self = this;
+        setInterval(function() {
+            self.age++;
+            console.log(self.age);
+        }, 1000)
+    }
+}
+var ragip = Object.create(User);
+ragip.startAging();
+
+// Arrow function -> it elleminates self. and it is more ellegant, so it makes cleaner code and use lexical THIS
+let User = {
+    name: 'Ragip Gashi', 
+    age: 40,
+    startAging: function() {
+        setInterval(() => {
+            this.age++;
+            console.log(this.age);
+        }, 1000)
+    }
+}
+let ragip = Object.create(User);
+ragip.startAging();
+*/
