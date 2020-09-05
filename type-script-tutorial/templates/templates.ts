329906152425b8f39ff41219f5a3b811599ab20d
Ragip Gashi <ragipgashi@gmial.com>
2020-09-06 00:33:12 +0200

    #1 Added some more examples to the templates.ts file to see the usage of startsWith(), endsWith() and includes().

--- type-script-tutorial/templates/templates.ts
+++ type-script-tutorial/templates/templates.ts
@@ -2,9 +2,19 @@ const greet = 'Hello World';
 const num1 = 5;
 const num2 = 3;
 
+// Using a function in template
 const total = (num1: number, num2: number): number => num1 + num2;
 
+// Creating a template
 const template = `${greet}, This is my template 
 in typescript. ${num1} + ${num2} = ${total(num1, num2)}`;
 
-console.log(template);
+//console.log(template);
+// Using startsWith() to verify if the template starts with a specific word
+console.log(template.startsWith('Hello'));
+
+// Using endsWith() to verify if the template ends with a specific word
+console.log(template.endsWith('8'));
+
+// Using includes() to see if the template contains a specific word
+console.log(template.includes('template'));
