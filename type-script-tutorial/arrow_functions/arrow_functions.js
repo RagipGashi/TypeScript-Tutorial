e7afd17b87b3ab30a3845b6bc7077dce9b65601b
Ragip Gashi <ragipgashi@gmial.com>
2020-09-05 22:37:58 +0200

    #1 Added an example in regular function and arrow function too

--- type-script-tutorial/arrow_functions/arrow_functions.js
+++ type-script-tutorial/arrow_functions/arrow_functions.js
@@ -9,4 +9,15 @@ console.log(getName('Ragip Gashi'));
 let getName = (name: string):string => name;
 
 console.log(getName('Ragip Gashi'));
-*/ 
+
+// Regular function
+let add = function(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+// Arrow function
+let add = (num1: number, num2: number): number => num1 + num2;
+
+console.log(add(1, 3));
+*/
+// 
