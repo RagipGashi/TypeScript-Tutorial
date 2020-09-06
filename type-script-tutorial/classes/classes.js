4c9b9b560edc254b73db0e44a6b21603a20228a9
Ragip Gashi <ragipgashi@gmial.com>
2020-09-06 13:42:03 +0200

    #1 Added classes folder with the proper files classes.ts and compiled classes.js too

---
+++ type-script-tutorial/classes/classes.js
@@ -0,0 +1,9 @@
+// Create a class
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    return Person;
+}());
+// Instantiate the class
+var john = new Person();
+console.log(john);
---
+++ type-script-tutorial/classes/classes.ts
@@ -0,0 +1,10 @@
+// Create a class
+class Person {
+    firstName: string;
+    lastName: string;
+    age: number
+}
+
+// Instantiate the class
+const john = new Person();
+console.log(john);
