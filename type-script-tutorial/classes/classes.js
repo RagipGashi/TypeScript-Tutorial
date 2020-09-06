21599458aa1a58e53b9292636c62d13fcb56c6b7
Ragip Gashi <ragipgashi@gmial.com>
2020-09-06 14:02:25 +0200

    #1 Added a method in classes.ts file and use its benefits

--- type-script-tutorial/classes/classes.js
+++ type-script-tutorial/classes/classes.js
@@ -5,12 +5,22 @@ var Person = /** @class */ (function () {
         this.lastName = last;
         this.age = age;
     }
+    // Creating a method which is very helpfull to use to different objects and use its properties
+    Person.prototype.sayHello = function () {
+        var output = this.firstName + ' says hello...';
+        console.log(output);
+        return output;
+    };
     return Person;
 }());
 // Instantiate the class
 var john = new Person('John', 'Doe', 30);
-console.log(john);
-console.log(john.firstName);
+// console.log(john);
+// console.log(john.firstName);
+// Calling the method sayHello() from john object
+john.sayHello();
 // Create another object
 var kevin = new Person('Kevin', 'Hart', 40);
-console.log(kevin);
+// console.log(kevin);
+// Calling the method sayHello() from kevin object
+kevin.sayHello();
--- type-script-tutorial/classes/classes.ts
+++ type-script-tutorial/classes/classes.ts
@@ -9,13 +9,24 @@ class Person {
         this.lastName = last;
         this.age = age;
     }
+
+    // Creating a method which is very helpfull to use to different objects and use its properties
+    sayHello() {
+        const output: string = this.firstName + ' says hello...';
+        console.log(output);
+        return output;
+    }
 }
 
 // Instantiate the class
 const john = new Person('John', 'Doe', 30);
-console.log(john);
-console.log(john.firstName);
+// console.log(john);
+// console.log(john.firstName);
+// Calling the method sayHello() from john object
+john.sayHello();
 
 // Create another object
 const kevin = new Person('Kevin', 'Hart', 40);
-console.log(kevin);
+// console.log(kevin);
+// Calling the method sayHello() from kevin object
+kevin.sayHello();
