7192b1fea1c1298cef148f8e2e9195fc5d127976
Ragip Gashi <ragipgashi@gmial.com>
2020-09-06 13:47:36 +0200

    #1 Added some example codes to see the benefits of using classes and constructors

--- type-script-tutorial/classes/classes.js
+++ type-script-tutorial/classes/classes.js
@@ -1,9 +1,16 @@
 // Create a class
 var Person = /** @class */ (function () {
-    function Person() {
+    function Person(first, last, age) {
+        this.firstName = first;
+        this.lastName = last;
+        this.age = age;
     }
     return Person;
 }());
 // Instantiate the class
-var john = new Person();
+var john = new Person('John', 'Doe', 30);
 console.log(john);
+console.log(john.firstName);
+// Create another object
+var kevin = new Person('Kevin', 'Hart', 40);
+console.log(kevin);
--- type-script-tutorial/classes/classes.ts
+++ type-script-tutorial/classes/classes.ts
@@ -3,8 +3,19 @@ class Person {
     firstName: string;
     lastName: string;
     age: number
+
+    constructor(first: string, last: string, age: number) {
+        this.firstName = first;
+        this.lastName = last;
+        this.age = age;
+    }
 }
 
 // Instantiate the class
-const john = new Person();
+const john = new Person('John', 'Doe', 30);
 console.log(john);
+console.log(john.firstName);
+
+// Create another object
+const kevin = new Person('Kevin', 'Hart', 40);
+console.log(kevin);
