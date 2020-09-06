e561497654c458db3637a9ad221dec5929e846d5
Ragip Gashi <ragipgashi@gmial.com>
2020-09-06 23:38:17 +0200

    #1 Added some more code to the modifiers.ts file to see the usage of protected modifier

--- type-script-tutorial/modifiers/modifiers.ts
+++ type-script-tutorial/modifiers/modifiers.ts
@@ -14,6 +14,7 @@ console.log(dog.name);
 */
 
 // Using private access modifier
+/*
 class Animal {
     // If we use privete, means we can have access only within the class, otherwise we need setter and getter
     private name: string;
@@ -39,3 +40,37 @@ const dog = new Animal('Skippy');
 dog.setName('Mimi');
 // Use getter method to get the name
 console.log(dog.getName());
+*/
+
+// Using protected access modifier
+class Animal {
+    // If we use protected, means we can have access within the class, and the classes that extends that class
+    protected name: string;
+
+    constructor (name: string) {
+        this.name = name;
+    }
+
+    // Using getter method
+    getName() {
+        return this.name; 
+    }
+
+    // Using setter method
+    setName(name: string) {
+        this.name = name;
+    }
+}
+
+// Create another class which extends Animal class to see the usage of protected modifier
+class Dog extends Animal {
+
+    constructor(name: string) {
+        // super must we write to call the constructor in the parent class
+        super(name);
+        console.log(this.name);
+    }
+}
+
+// Instantiate the class Dog which extends Animal
+const dog: Animal = new Dog('Ralph');
