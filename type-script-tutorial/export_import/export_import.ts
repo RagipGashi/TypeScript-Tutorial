d0260aa9b4fb4295149b39af410699a06f1d06db
Ragip Gashi <ragipgashi@gmial.com>
2020-09-07 10:09:56 +0200

    #1 Added export_import folder with its files to see the usage of export and import files from one another

---
+++ type-script-tutorial/export_import/export_import.ts
@@ -0,0 +1,12 @@
+// Import all the classes and interfaces form the file shapes with (*)
+import * as shapes from './shapes'
+
+// Instantiate the class Triangle
+const t = new shapes.Triangle();
+t.setPoints(3);
+t.getPoints();
+
+// Instantiate the class Square
+const s = new shapes.Square();
+s.setPoints(4);
+s.getPoints();
